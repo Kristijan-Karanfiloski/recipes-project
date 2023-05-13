012f1d5876958b03d8ac86a42dc03b563d05f2f9
Kristijan <kikaccc29@gmail.com>
2023-05-13 16:06:06 +0200

    "Added Observables"

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,27 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../services/shopping-list.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit,OnDestroy {
   ingredients: any = [];
+  private idChangeSub:Subscription
 
   constructor(
     private shoppingList: ShoppingListService,
-    private ingredientAdded: ShoppingListService
+
   ) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingList.getIngredients();
-    this.shoppingList.ingredientsChanged.subscribe(
+    this.idChangeSub=this.shoppingList.ingredientsChanged
+      .subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
   }
+
+  ngOnDestroy() {
+    this.idChangeSub.unsubscribe()
+  }
+
+
 }
